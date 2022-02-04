8c5752c00cd483d48b9f16e044161452a30ba183
Dimitar Atanasov <75276016+datanasovv@users.noreply.github.com>
2022-02-04 16:12:18 +0200

    navbar and theme context created

---
+++ react-blog-app/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
+
+function App() {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <div className={`container ${theme}`}>
+      <Navbar />
+      <div className="main">
+        <h1>Posts</h1>
+        <ul>
+          <li>
+            <h2>Post 1</h2>
+            <p>post 1 content</p>
+          </li>
+          <li>
+            <h2>Post 2</h2>
+            <p>post 2 content</p>
+          </li>
+        </ul>
+      </div>
+      <div className="footer">Awesome blog. All rights reserved</div>
+    </div>
+  );
+}
+
+export default App;
